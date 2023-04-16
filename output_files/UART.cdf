a2cacb1a53aa899c403d0b8ba1a8ae234bd7ee36
ddsand <devonsandhu3@gmail.com>
2023-04-16 13:08:16 -0700

    debug uarst

---
+++ output_files/UART.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.0 Build 711 06/05/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("C:/Users/yourf/Desktop/ENSC350/Project/Customized-UART-Protocol-FPGA/output_files/") File("UART.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- tb_Transmitter.vhd
+++ tb_Transmitter.vhd
@@ -13,10 +13,7 @@ COMPONENT Transmitter is
      PORT(
 		  KEY		  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
 		  CLOCK_50 : IN STD_LOGIC;
-        UART_RTS : IN STD_LOGIC;
-		  UART_RTX : IN STD_LOGIC;
 		  
-		  LEDG	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
 		  UART_CTS : OUT STD_LOGIC;
 		  UART_TXD : OUT STD_LOGIC); 
 END COMPONENT;
@@ -26,14 +23,14 @@ END COMPONENT;
 CONSTANT period : time := 10ns;
 SIGNAL clksig : STD_LOGIC := '1'; 
 
-SIGNAL UART_RTSsig, UART_RTXsig, UARTTXDSIG : STD_LOGIC;
+SIGNAL UARTTXDSIG : STD_LOGIC;
 SIGNAL KEYsig : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
 BEGIN
 
 DUT : Transmitter 
 
-PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, UART_RTS=>UART_RTSsig, UART_RTX=>UART_RTXsig, UART_TXD => UARTTXDSIG);
+PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, UART_TXD => UARTTXDSIG);
 
 clksig <= NOT clksig after period;
 
@@ -48,7 +45,7 @@ wait for PERIOD;
 
 Keysig(3) <= '1';
 
-wait for 2500ns;
+wait for 30000ns;
 
 wait;
 
