fe6a7bbab633aab60aa8c6c52aa1884720fea350
Rajnesh Joshi <rajnesh.joshi28@gmail.com>
2023-04-16 18:47:02 -0700

    Receiver debug progress - serial port

--- tb_Receiver.vhd
+++ tb_Receiver.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
-	
+    
 ENTITY tb_RECEIVER is
 --No inputs or outputs
 END tb_receiver;
@@ -11,16 +11,16 @@ ARCHITECTURE test OF tb_receiver IS
 
 COMPONENT receiver is
      port(
-		  KEY		  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-		  CLOCK_50 : IN STD_LOGIC;
+          KEY          : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+          CLOCK_50 : IN STD_LOGIC;
         UART_RTS : IN STD_LOGIC;
-		  UART_RTX : IN STD_LOGIC;--
-		  
-		  LEDG	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-		  UART_CTS : OUT STD_LOGIC;
-		  UART_TXD : OUT STD_LOGIC;
-		  DATA	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-);		
+          UART_RXD : IN STD_LOGIC;--
+          
+          LEDG      : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+          UART_CTS : OUT STD_LOGIC;
+          UART_TXD : OUT STD_LOGIC;
+          DATA      : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+);        
 END COMPONENT;
 
 
@@ -35,7 +35,7 @@ BEGIN
 
 DUT : receiver 
 
-PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, UART_RTS=>UART_RTSsig, UART_RTX=>UART_RTXsig, DATA => DATASIG);
+PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, UART_RTS=>UART_RTSsig, UART_RXD=>UART_RTXsig, DATA => DATASIG);
 
 clksig <= NOT clksig after period;
 
@@ -94,7 +94,7 @@ UART_RTXsig <= '0';
 wait for 52100ns;
 
 Keysig(3) <= '1';
-UART_RTXsig <= '1';		--stop bit
+UART_RTXsig <= '1';        --stop bit
 
 wait for 2500ns;
 
