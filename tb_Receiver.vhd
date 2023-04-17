807e1598841ea304b3db2c47d26114bfbf1ebcaf
Rajnesh Joshi <rajnesh.joshi28@gmail.com>
2023-04-17 01:27:06 -0700

    Receiver verified

--- tb_Receiver.vhd
+++ tb_Receiver.vhd
@@ -10,17 +10,15 @@ END tb_receiver;
 ARCHITECTURE test OF tb_receiver IS
 
 COMPONENT receiver is
-     port(
-          KEY          : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-          CLOCK_50 : IN STD_LOGIC;
-        UART_RTS : IN STD_LOGIC;
-          UART_RXD : IN STD_LOGIC;--
-          
-          LEDG      : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-          UART_CTS : OUT STD_LOGIC;
-          UART_TXD : OUT STD_LOGIC;
-          DATA      : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-);        
+	PORT(
+		  KEY		  			  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		  CLOCK_50 			  : IN STD_LOGIC;
+		  
+		  UART_RXD 			  : IN STD_LOGIC;
+        UART_CTS 			  : OUT STD_LOGIC;
+		  
+		  recievedMessage	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+);		
 END COMPONENT;
 
 
@@ -28,14 +26,15 @@ END COMPONENT;
 CONSTANT period : time := 10ns;
 SIGNAL clksig : STD_LOGIC := '1'; 
 
-SIGNAL UART_RTSsig, UART_RTXsig : STD_LOGIC;
+SIGNAL UART_RXDsig, UART_CTSsig : STD_LOGIC;
 SIGNAL KEYsig : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
 SIGNAL DATASIG : STD_LOGIC_VECTOR(7 DOWNTO 0);
 BEGIN
 
 DUT : receiver 
 
-PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, UART_RTS=>UART_RTSsig, UART_RXD=>UART_RTXsig, DATA => DATASIG);
+PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, UART_RXD=>UART_RXDsig, UART_CTS=>UART_CTSsig, recievedMessage => DATASIG);
 
 clksig <= NOT clksig after period;
 
@@ -45,58 +44,47 @@ PROCESS
 BEGIN
 
 Keysig(3) <= '0';
-UART_RTXsig <= '1';
-
+UART_RXDsig <= '1';
 wait for 100ns;
 
 Keysig(3) <= '1';
-UART_RTXsig <= '0';
+UART_RXDsig <= '0';
+wait for 78160ns;
 
-wait for 72100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '1';
 
+UART_RXDsig <= '1';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '0';
-
+UART_RXDsig <= '0';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '1';
+UART_RXDsig <= '1';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '0';
-
+UART_RXDsig <= '0';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '1';
-
+UART_RXDsig <= '1';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '0';
-
+UART_RXDsig <= '0';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '1';
-
+UART_RXDsig <= '1';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '0';
-
+UART_RXDsig <= '0';
 wait for 52100ns;
 
-Keysig(3) <= '1';
-UART_RTXsig <= '1';        --stop bit
 
-wait for 2500ns;
+
+
+UART_RXDsig <= '1';
+wait for 52100ns;
+
+UART_RXDsig <= '1';        --stop bit
+wait for 500000NS;
 
 wait;
 
---
+++ tb_UART.vhd
@@ -0,0 +1,62 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+ 
+LIBRARY WORK;
+USE WORK.ALL;
+
+ENTITY tb_UART IS
+END tb_UART;
+
+ARCHITECTURE test of tb_UART IS
+COMPONENT simulatedUART
+	PORT(   CLOCK_50 	: IN STD_LOGIC; -- the fast clock for spinning wheel
+			  KEY 		: IN STD_LOGIC_VECTOR(3 downto 0);  -- includes slow_clock and reset
+			  
+			  MESSAGE1   		  : IN STD_LOGIC_VECTOR(7 downto 0);
+			  MESSAGE2   		  : IN STD_LOGIC_VECTOR(7 downto 0);
+			  
+			  recievedMessage1	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			  recievedMessage2	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END COMPONENT;
+
+
+
+
+CONSTANT period : time := 10ns;
+SIGNAL clksig : STD_LOGIC := '1'; 
+
+SIGNAL MESSAGE1SIG   		  :  STD_LOGIC_VECTOR(7 downto 0);
+SIGNAL MESSAGE2SIG   		  :  STD_LOGIC_VECTOR(7 downto 0);
+
+SIGNAL KEYsig : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+SIGNAL recievedMessage1SIG	  :  STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL recievedMessage2SIG	  :  STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+
+BEGIN
+
+DUT : simulatedUART 
+
+PORT MAP(KEY=>KEYsig, CLOCK_50=>clksig, MESSAGE1=>MESSAGE1SIG, MESSAGE2=>MESSAGE1SIG, recievedMessage1 => recievedMessage1SIG, recievedMessage2 => recievedMessage2SIG);
+
+clksig <= NOT clksig after period;
+
+PROCESS
+
+BEGIN
+
+Keysig(3) <= '0';
+
+wait for 40ns;
+
+Keysig(3) <= '1';
+MESSAGE1SIG <= "10001000";
+MESSAGE2SIG <= "10101010";
+
+WAIT FOR 600000ns;
+
+WAIT;
+END PROCESS;
+END test;
