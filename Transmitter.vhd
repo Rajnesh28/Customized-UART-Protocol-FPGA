27e6af151726b5e0a9c31f30d672afed47cf68c5
Rajnesh Joshi <rajnesh.joshi28@gmail.com>
2023-04-16 23:32:30 -0700

    UART module

--- Transmitter.vhd
+++ Transmitter.vhd
@@ -2,91 +2,117 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 
---generic (
---	data_width : integer := 8;
---	baud_rate  : integer := 19200;
---	cycles_per_bit : integer := 2605;
---	);
 	
 entity TRANSMITTER is
-	PORT(   CLOCK_50 	: IN STD_LOGIC; 
-			  KEY 		: IN STD_LOGIC_VECTOR(3 downto 0);  
-			  
-			  UART_TXD	: OUT STD_LOGIC;
-			  UART_CTS	: OUT STD_LOGIC;
-			  LEDG : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+     port(
+		  KEY		  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		  CLOCK_50 : IN STD_LOGIC;
+		  MESSAGE  : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		  
+        UART_RTS : IN STD_LOGIC;
+		  UART_TXD : OUT STD_LOGIC;
+		  
+		  LEDG	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+		  ); 
 end TRANSMITTER;
 
 architecture RTL of TRANSMITTER IS
 
-TYPE STATE_TYPE IS (init, transmit, endTransmit);
-SIGNAL transmitState : STATE_TYPE;
+TYPE STATE_TYPE IS (idle, calculateParity, beginTransmit, transmit, endTransmit);
+SIGNAL TRANSMIT_STATE : STATE_TYPE;
 
 BEGIN
-implicitFSM: PROCESS(CLOCK_50, KEY(3))
 
-VARIABLE memoryCounter : INTEGER RANGE 0 TO 256;
-VARIABLE messageByteCounter : INTEGER Range 0 TO 256;
-VARIABLE messageByte : STD_LOGIC_VECTOR(7 DOWNTO 0);
+implicitFSM: PROCESS(CLOCK_50)
+
 VARIABLE K : INTEGER RANGE 0 TO 11;
 
-VARIABLE parityBit : STD_LOGIC;
-VARIABLE stopBit : STD_LOGIC;
-VARIABLE startBit : STD_LOGIC;
-VARIABLE DATA_CYCLES : INTEGER RANGE 0 to 2700;
-VARIABLE COUNTER : INTEGER RANGE 0 TO 100000000;
-VARIABLE beginTransmit : STD_LOGIC;
-VARIABLE messageToTransmit : STD_LOGIC_VECTOR (10 downto 0);
+VARIABLE stopBit 		: STD_LOGIC;
+VARIABLE startBit 	: STD_LOGIC;
+VARIABLE dataCycles  : INTEGER RANGE 0 to 2700;
+VARIABLE COUNTER   	: INTEGER RANGE 0 TO 100000000;
+VARIABLE parityBit 	: STD_LOGIC := '1';
+
+VARIABLE INDEX					: INTEGER RANGE 0 TO 15;
+VARIABLE messageToTransmit : STD_LOGIC_VECTOR (10 DOWNTO 0);
 
 BEGIN
 
 IF (KEY(3) = '0') THEN
-	UART_TXD <= '1';
-	COUNTER := 0;
-	K := 0;
-	DATA_CYCLES := 0;
-	messageToTransmit := "00000000000";
-	LEDG(0) <= '1';
-	transmitState <= init;
+	TRANSMIT_STATE <= idle;
 	
 ELSIF(RISING_EDGE(CLOCK_50)) THEN
-	CASE transmitState IS 
 	
-	WHEN init =>
-		--messageByte := "01000001";
-		parityBit := '1';
+	CASE TRANSMIT_STATE IS 
+
+		
+	WHEN idle =>
+	
+		COUNTER := 0;
+		K := 0;
+		dataCycles := 0;
+		index := 0;
+		
 		startBit := '0';
 		stopBit := '1';
-		UART_CTS <= '1';
+		
 		UART_TXD <= '1';
 		
-		LEDG(0) <= '0';
-
-		messageToTransmit :=  stopBit & PARITYBIT & "01001000" & startbit;	--its transmitting backwards!!!!
+		IF (UART_RTS = '1') THEN 
+			TRANSMIT_STATE <= calculateParity;
+		ELSE 
+			TRANSMIT_STATE <= idle;
+		END IF;
+		
+	WHEN calculateParity => 
+	
+		IF (message(INDEX) = '1') THEN
+			parityBit := NOT parityBit;
+		ELSE
+			parityBit := parityBit;
+		END IF;
+		
+		IF (INDEX < 7)THEN
+			INDEX := INDEX + 1;
+			TRANSMIT_STATE <= calculateParity;
+		ELSE 
+			index := 0;
+			TRANSMIT_STATE <= beginTransmit;
+		END IF;
+			
+		
+	WHEN beginTransmit =>
+	
+		messageToTransmit := startbit & message & parityBit & stopBit;
 		UART_TXD <= messageToTransmit(K);
-		transmitState<= transmit;
+		
+		TRANSMIT_STATE <= transmit;
 		
 	WHEN transmit =>
-		beginTransmit := '1';
+		IF (dataCycles = 10) THEN
 		
-	IF (DATA_CYCLES = 2605) THEN
-		IF (K < 10) THEN
-			K := K + 1;
-			UART_TXD <= messageToTransmit(K);
-			DATA_CYCLES := 0;
-			TRANSMITSTATE <= TRANSMIT;
-		ELSIF (K = 10) THEN
-			UART_TXD <= messageToTransmit(K);
-			TRANSMITSTATE <= ENDTRANSMIT;
-		END IF;
-	ELSE	
-		DATA_CYCLES := DATA_CYCLES + 1;
-		transmitState <= transmit;
+			IF (K < 10) THEN
+				K := K + 1;
+				UART_TXD <= messageToTransmit(K);
+				dataCycles := 0;
+				
+				TRANSMIT_STATE <= TRANSMIT;
+				
+			ELSIF (K = 10) THEN
+				UART_TXD <= messageToTransmit(K);
+				
+				TRANSMIT_STATE <= endTransmit;
+				
+			END IF;
+			
+		ELSE	
+			dataCycles := dataCycles + 1;
+			TRANSMIT_STATE <= transmit;
 		END IF;
 
 		
 	WHEN endTransmit =>
-		transmitState <= endTransmit;
+		TRANSMIT_STATE <= idle;
 	
 	
 	END CASE;
--- UART.vhd
+++ UART.vhd
@@ -9,12 +9,17 @@ ENTITY UART IS
 	PORT(   CLOCK_50 	: IN STD_LOGIC; -- the fast clock for spinning wheel
 			  KEY 		: IN STD_LOGIC_VECTOR(3 downto 0);  -- includes slow_clock and reset
 			  
-			  UART_RTX  : IN STD_LOGIC;
+			  MESSAGE   : IN STD_LOGIC_VECTOR(7 downto 0);
+			  
+			  UART_RXD  : IN STD_LOGIC;
 			  UART_RTS  : IN STD_LOGIC;
 			  
 			  
 			  UART_TXD	: OUT STD_LOGIC;
-			  UART_CTS	: OUT STD_LOGIC
+			  UART_CTS	: OUT STD_LOGIC;
+			  
+			  recievedMessage	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+	
 			  );
 END UART;
 
@@ -22,26 +27,35 @@ END UART;
 ARCHITECTURE structural OF UART IS
 
 COMPONENT TRANSMITTER is
-	PORT(   CLOCK_50 	: IN STD_LOGIC; 
-			  KEY 		: IN STD_LOGIC_VECTOR(3 downto 0);  
-			  
-			  UART_TXD	: OUT STD_LOGIC;
-			  UART_CTS	: OUT STD_LOGIC);
+     port(
+		  KEY		  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		  CLOCK_50 : IN STD_LOGIC;
+		  MESSAGE  : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		  
+        UART_RTS : IN STD_LOGIC;
+		  UART_TXD : OUT STD_LOGIC;
+		  
+		  LEDG	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+		  ); 
 END COMPONENT;
 
 COMPONENT RECEIVER is
-	PORT(   CLOCK_50 	: IN STD_LOGIC;
-			  KEY 		: IN STD_LOGIC_VECTOR(3 downto 0);  
-			  
-			  UART_RTX  : IN STD_LOGIC;
-			  UART_RTS  : IN STD_LOGIC);
+	PORT(
+		  KEY		  			  : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		  CLOCK_50 			  : IN STD_LOGIC;
+		  
+		  UART_RXD 			  : IN STD_LOGIC;
+        UART_CTS 			  : OUT STD_LOGIC;
+		  
+		  recievedMessage	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+);		
 end COMPONENT;
 
 
 BEGIN
 
-UART_T: TRANSMITTER PORT MAP (CLOCK_50 => CLOCK_50, KEY => KEY, UART_TXD => UART_TXD, UART_CTS => UART_CTS);
+UART_T: TRANSMITTER PORT MAP (CLOCK_50 => CLOCK_50, KEY => KEY, UART_TXD => UART_TXD, UART_RTS => UART_RTS, message => message);
 
-UART_R: RECEIVER PORT MAP (CLOCK_50 => CLOCK_50, KEY => KEY, UART_RTX => UART_RTX, UART_RTS => UART_RTS);
+UART_R: RECEIVER PORT MAP (CLOCK_50 => CLOCK_50, KEY => KEY, UART_RXD => UART_RXD, UART_CTS => UART_CTS, recievedMessage => recievedMessage);
 
 END;
